d79d13bf398f7a33ecda50193fffce3ad0d8d668
Mathias <mathias.dupont50@gmail.com>
2018-10-16 17:58:33 +0200

    new page, adding back-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,40 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { TaskProvider } from '../providers/task/task';
+import { FormsModule } from "@angular/forms";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item/add-item'
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddItemPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    FormsModule,
+    HttpClientModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddItemPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    TaskProvider,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import {NavController} from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item'
+import { TaskProvider } from "../../providers/task/task";
+import {Task} from "../../models/task";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage implements OnInit{
 
+  constructor(public navCtrl: NavController, private taskProvider : TaskProvider) {
+  }
+
+  tasks: Task[] = [];
+
+  ngOnInit(){
+    this.taskProvider.getTasks().then(
+      tasks => {
+        if(null !== tasks){
+          this.tasks = tasks;
+        }
+      }
+    )
+  }
+
+  goToAddTasks(){
+    this.navCtrl.push(AddItemPage);
+  }
+
+  delete(tasks){
+    this.taskProvider.removeTasks(tasks);
   }
 
 }

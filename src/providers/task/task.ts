56dad8201aef9b133ed33413a1ff8d408a2c26c2
Mathias <mathias.dupont50@gmail.com>
2018-10-16 17:59:48 +0200

    upload back-end

---
+++ src/providers/task/task.ts
@@ -0,0 +1,28 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {Task} from "../../models/task";
+import { Storage } from '@ionic/storage';
+
+
+@Injectable()
+export class TaskProvider {
+
+  constructor(public http: HttpClient, public  storage: Storage) {
+    console.log('Hello TaskProvider Provider');
+  }
+
+  //Sauvegarder les taches
+  saveTasks(tasks: Task[]) {
+    this.storage.set('tasks', tasks);
+  }
+
+  //Récuperer les taches sauvegardées
+  getTasks(): Promise<Task[]> {
+    return this.storage.get('tasks');
+  }
+
+  removeTasks(tasks){
+    this.storage.remove('tasks');
+  }
+
+}
